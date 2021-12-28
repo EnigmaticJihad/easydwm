2e27801374adfa5d3166e52f6d2cec9cb79c1a5f
Enigmatic Jihad <enigmatic.jihad@gmail.com>
2021-12-29 04:15:35 +0600

    pywal custom templates

---
+++ .config/wal/templates/dwm.h
@@ -0,0 +1,39 @@
+static char normfgcolor[]                = "{color15}";
+static char normbgcolor[]                = "{color0}";
+static char normbordercolor[]            = "{color8}";
+static char normfloatcolor[]             = "{color0}";
+
+static char selfgcolor[]                 = "{color15}";
+static char selbgcolor[]                 = "{color2}";
+static char selbordercolor[]             = "{color15}";
+static char selfloatcolor[]              = "{color2}";
+
+static char titlenormfgcolor[]           = "{color15}";
+static char titlenormbgcolor[]           = "{color0}";
+static char titlenormbordercolor[]       = "{color8}";
+static char titlenormfloatcolor[]        = "{color0}";
+
+static char titleselfgcolor[]            = "{color15}";
+static char titleselbgcolor[]            = "{color2}";
+static char titleselbordercolor[]        = "{color15}";
+static char titleselfloatcolor[]         = "{color2}";
+
+static char tagsnormfgcolor[]            = "{color15}";
+static char tagsnormbgcolor[]            = "{color0}";
+static char tagsnormbordercolor[]        = "{color8}";
+static char tagsnormfloatcolor[]         = "{color0}";
+
+static char tagsselfgcolor[]             = "{color15}";
+static char tagsselbgcolor[]             = "{color2}";
+static char tagsselbordercolor[]         = "{color15}";
+static char tagsselfloatcolor[]          = "{color2}";
+
+static char hidnormfgcolor[]             = "{color15}";
+static char hidselfgcolor[]              = "{color15}";
+static char hidnormbgcolor[]             = "{color0}";
+static char hidselbgcolor[]              = "{color2}";
+
+static char urgfgcolor[]                 = "{color15}";
+static char urgbgcolor[]                 = "{color1}";
+static char urgbordercolor[]             = "{color1}";
+static char urgfloatcolor[]              = "{color1}";
